cb62f2cd120bbf99f3de21a2478e271e4553440f
Olga <seredaolga03@gmail.com>
2022-12-01 14:20:56 +0200

    proj_v.1.2

--- app/src/main/java/com/university/project_1_2/presentation/fragments/HdetailFragment.kt
+++ app/src/main/java/com/university/project_1_2/presentation/fragments/HdetailFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.asLiveData
 import com.university.project_1_2.data.dbs.MainDB
@@ -17,6 +18,11 @@ import com.university.project_1_2.databinding.FragmentHdetailBinding
 class HdetailFragment : Fragment() {
     private lateinit var binding: FragmentHdetailBinding
     private var ID = 1
+    private var curID = 0
+    private var curANIME = ""
+    private var curCHARACTER = ""
+    private var curQUOTE = ""
+    private var curTIME = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,6 +36,10 @@ class HdetailFragment : Fragment() {
         ID = arguments?.getInt("hID")!!
         val context = context
         ShowQuote(context!!)
+        binding.imSaveQuote.setOnClickListener{
+            SaveQuote(context)
+            Toast.makeText(context!!,"Saved!", Toast.LENGTH_SHORT).show()
+        }
     }
     private fun ShowQuote(context: Context){
         val db = MainDB.getDB(context)
@@ -39,7 +49,27 @@ class HdetailFragment : Fragment() {
             }
         }
     }
+    private fun SaveQuote(context: Context){
+        val db = MainDB.getDB(context)
+        val saved = Saved(
+            curID,
+            curANIME,
+            curCHARACTER,
+            curQUOTE,
+            curTIME
+        )
+        var threadInsert5 = Thread {
+            db.getDaoSaved().insertSaved(saved)
+        }
+        threadInsert5.start()
+        threadInsert5.join()
+    }
     private fun showData(data: History)= with(binding){
+        curID = data.id!!
+        curANIME = data.anime
+        curCHARACTER = data.character
+        curQUOTE = data.quote
+        curTIME = data.time
         tvAnimeTitle.text = data.anime
         tvCharacter.text = "-${data.character}-"
         tvQuote.text = "''${data.quote}''"
